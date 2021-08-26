063e0c5077a1929600f508d9a1280dd30be96e41
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-08-27 00:00:54 +0200

    all at once

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *Main entry point
+ *
+ *Description: Prints "Programing is like a multilingual puzle"
+ *
+ *Return: Always 0 for success
+ */
+int main(void)
+{
+  puts("\"Programing is like a multilingual puzzle");
+  return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *Main Entry point
+ *
+ *Description: Prints the size of various types on the computer it is
+ *compiled on and run on
+ *
+ *Returns: 0 for success
+ */
+int main(void)
+{
+  printf("size of a char: %d byte(s)\n", (int) sizeof(char));
+  printf("size of an int: %d byte(s)\n", (int) sizeof(int));
+  printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
+  printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+  printf("size of a float: %d byte(s)\n", (int) sizeof(float));
+  
+  return (0);
+}

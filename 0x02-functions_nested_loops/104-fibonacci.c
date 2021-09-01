0813a1108e607522ad30b6813bdc9c747101cfe8
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-02 00:25:27 +0200

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * main - print fibonacci
  *
- * Return: Always 0.
+ * Return: always 0
  */
-
 int main(void)
 {
-long int sum;
-long int first, second;
-int count;
+	unsigned long int a, a1, a2, b, b1, b2, c, c1, c2, d, e;
 
-sum = 0;
-first = 0;
-second = 1;
-count = 0;
+	a = 1;
+	b = 2;
+	c = a + b;
 
-while (count < 99)
-{
-sum = first + second;
-printf("%li, ", sum);
-first = second;
-second = sum;
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+	for (d = 3; d < 89; d++)
+	{
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
+	for (e = 89; e < 98; e++)
+	{
+		printf("%lu%lu, ", c1, c2);
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+		c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+		c2 = (a2 + b2) % 1000000000;
+	}
+printf("%lu%lu\n", c1, c2);
 
-count++;
-}
-sum = first + second;
-printf("%li\n", sum);
-
-return (0);
+	return (0);
 }

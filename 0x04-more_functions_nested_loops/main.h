995df0665cffc385c3bc95e85d8fbc4c450221ca
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-02 14:16:39 +0200

    0x04

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,10 +4,10 @@
 int _putchar(char);
 
 /* 0-isupper.c */
-int _isupper(int);
+int _isupper(int c);
 
 /* 1-isdigit.c */
-int _isdigit(int);
+int _isdigit(int c);
 
 /* 2-mul.c */
 int mul(int a, int b);

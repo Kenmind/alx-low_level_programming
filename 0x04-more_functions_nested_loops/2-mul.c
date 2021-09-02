8a38a4dfd9b605f1db383bb15b04f2037a170049
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-02 17:24:38 +0200

    0x04

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * Return: 0
+ * @a: first integer
+ * @b: second integer
+ */
+
+int mul(int a, int b)
+{
+_putchar(a * b);
+return (0);
+}

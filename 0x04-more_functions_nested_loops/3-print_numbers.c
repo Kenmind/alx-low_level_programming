03c9c6d312f1b0ebaa7ab299fb322f07d1d66090
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-02 16:32:24 +0200

    0x04

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * _putchar: prints to standard output
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+_putchar(c++);
+}
+_putchar(c);
+
+}

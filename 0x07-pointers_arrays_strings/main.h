90e48c6d16e418b2c858ffe2b9e31e14bd268ce1
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-13 14:54:46 +0100

    0x07 3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-
 int _putchar(char c);
 
 /* 0-memset.c */
@@ -17,19 +15,19 @@ char *_strchr(char *s, char c);
 /* 3-strspn.c */
 unsigned int _strspn(char *s, char *accept);
 
-/* 4-strpbrk */
+/* 4-strpbrk.c */
 char *_strpbrk(char *s, char *accept);
 
 /* 5-strstr.c */
-char *strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 
-/* 6-print_chessboard.c */
+/* 7-print_chessboard.c */
 void print_chessboard(char (*a)[8]);
 
 /* 8-print_diagsums.c */
 void print_diagsums(int *a, int size);
 
-/* 100-set_string.c */
+/* 9-set_string.c */
 void set_string(char **s, char *to);
 
 #endif

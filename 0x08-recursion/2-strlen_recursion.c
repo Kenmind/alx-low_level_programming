6cee868ea7716ee3a74d50dc53f65ce5befd96a3
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-15 17:01:51 +0100

    0x08 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (*s);
-else
-s++;
-
-_strlen_recursion(s + 1);
-
-return (0);
+int i = 0;
+if (*s)
+{
+i++;
+i += _strlen_recursion(s + 1);
+}
+return (i);
 }

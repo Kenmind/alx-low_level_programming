761dc55fe5db234c636c4587eff2a3b01ddcb991
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-17 12:08:12 +0100

    0x0A betty style on task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,20 +10,19 @@
 
 int main(int argc, char *argv[])
 {
-int i, prod = 1;
+int i, z, prod;
 
-if (argc > 1 && argc <= 3)
-{
-for (i = 1; i < argc; i++)
-{
-prod *= atoi(argv[i]);
-}
-printf("%d\n", prod);
-}
-else
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+
+i = atoi(argv[1]);
+z = atoi(argv[2]);
+prod = i *z;
+
+printf("%d\n", prod);
+
 return (0);
 }

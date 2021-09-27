b3e1da8ba63978582cfab561f2bbf7c10478290a
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-09-27 12:49:57 +0100

    0x0D completed task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of the file it was compiled from followed by a newline
+ * Return: void
+ */
+
+void main(void)
+{
+	printf("%s\n", __FILE__);
+}

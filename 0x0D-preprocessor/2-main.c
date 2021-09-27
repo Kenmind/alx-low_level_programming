745cbcc3c4e45271a706998bacdefb19120dfa62
Kenmind <62250484+Kenmind@users.noreply.github.com>
2021-09-27 18:40:03 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "dog.h"
 
 /**
  * main - prints name of the file it was compiled from followed by a newline
  * Return: void
  */
 
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);

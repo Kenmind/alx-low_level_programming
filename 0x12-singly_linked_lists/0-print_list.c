8bcfc95f75198bd99432bdab00e5a8d79068dffe
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-10-07 19:25:21 +0100

    Added \n on return for NULL in 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)");
+			printf("[0] (nill)\n");
 		else
 			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;

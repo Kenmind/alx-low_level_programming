0b0b7e4afd31e1dda0ae44043b9ab57b825f2678
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-25 16:07:27 +0000

    finished #11

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,53 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
 /**
- * main - Generates random valid passwords for the program 101-crackme
- * Retur: Always 0
+ * main - generates password for cracme
+ * Return: 0
  */
 
 int main(void)
 {
-char password[84];
-int index = 0, sum = 0, diff_half1, diff_half2;
+	char a[10], c[10];
+	int i = 0, j = 0, k = 0;
 
-srand(time(0));
+	while (!k)
+		k = strlen(a);
+	i = k;
+	j = 0;
+	a[i] = k;
+	while (i >= 0)
+	{
+		c[j] = a[i] + i;
+		i--;
+		j++;
+	}
+	c[j] = 0;
+	printf("%s\n", c);
 
-while (sum < 2772)
-{
-password[index] = 33 + rand() % 94;
-sum += password[index++];
-}
-
-password[index] = '\0';
-
-if (sum != 2772)
-{
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half1))
-{
-password[index] -= diff_half1;
-break;
-}
-}
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half2))
-{
-password[index] -= diff_half2;
-break;
-}
-}
-}
-
-printf("%s", password);
-
-return (0);
+	return (0);
 }

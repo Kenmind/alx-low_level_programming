ec65e6af347129157b8bd9ee0930bac5dedd7773
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-11-25 15:00:34 +0000

    corrected #1 && #2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,7 +9,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t size = 0;
-	
+
 	while (current != NULL)
 	{
 		current = current->next;
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,5 +22,4 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	*head = newNode;
 
 	return (newNode);
-	
 }

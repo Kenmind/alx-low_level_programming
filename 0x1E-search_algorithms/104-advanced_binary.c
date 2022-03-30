b0328ce3f9a48fe0eb4007024e4cb41f346a0e31
Kenmind <kennedykalaluka04@gmail.com>
2022-03-30 08:43:50 +0200

    Use recursion

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,27 +1,7 @@
 #include "search_algos.h"
 
 /**
- * print_subarray - prints a subarray
- * @array: pointer to array
- * @first: index of first element to print
- * @last: index of last element to print
- */
-
-void print_subarray(int *array, size_t first, size_t last)
-{
-	size_t i = 0;
-
-	printf("Searching in array: ");
-	for (i = first; i < last; i++)
-	{
-		printf("%d, ", array[i]);
-	}
-	printf("%d\n", array[i]);
-}
-
-
-/**
- * bin_search_mod - search for val in sorted int array using Binary search alg
+ * bin_recursive - search for val in sorted int array using Binary search alg
  * @array: pointer to int array
  * @value: value to find
  * @first: first index of subarray
@@ -29,37 +9,22 @@ void print_subarray(int *array, size_t first, size_t last)
  * Return: index of first match or -1 if not found
  */
 
-int bin_search_mod(int *array, int value, int first, int last)
+int bin_recursive(int *array, size_t first, size_t last, int value)
 {
-	size_t mid = 0;
+	size_t i;
 
-	while (first <= last)
-	{
-		print_subarray(array, first, last);
-		mid = (first + last) / 2;
-		if (array[mid] > value)
-		{
-			last = mid - 1;
-			bin_search_mod(array, value, first, last);
-
-		}
-		else if (array[mid] < value)
-		{
-			first = mid + 1;
-			bin_search_mod(array, value, first, last);
-		}
-		else
-		{
-			if (array[mid - 1] != value)
-				return (mid);
-			while (array[mid] == value)
-			{
-				mid -= 1;
-			}
-			return (mid + 1);
-		}
-	}
-	return (-1);
+	if (last < first)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = first; i < last; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+	i = first + (last - first) / 2;
+	if (array[i] == value && (i == first || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (bin_recursive(array, first, i, value));
+	return (bin_recursive(array, i + 1, last, value));
 }
 
 
@@ -73,13 +38,7 @@ int bin_search_mod(int *array, int value, int first, int last)
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t first = 0;
-	size_t last = 0;
-
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 1)
 		return (-1);
-
-	last = size - 1;
-	return (bin_search_mod(array, value, first, last));
-
+	return (bin_recursive(array, 0, size - 1, value));
 }
